8030cac7d0e64902c9803eb85d4eb37ff6451814
Olajide-Ibukunoluwa-Temitope <31543991+Olajide-Ibukunoluwa-Temitope@users.noreply.github.com>
2022-05-24 13:49:18 +0100

    comment

--- src/components/custom/Button/Button.tsx
+++ src/components/custom/Button/Button.tsx
@@ -14,7 +14,7 @@ const Button: React.FC<ButtonProps> = ({
   return (
     <button
       className="btn"
-      style={{ width, height, backgroundColor: bgColor, fontSize, color }}
+      style={{ width, height, backgroundColor: bgColor, fontSize, color }} // come back to replace in-line style with better alternative
       onClick={() => onClick?.()}
     >
       <p className="btn_text">{btnText ? btnText : "LEARN MORE"}</p>
